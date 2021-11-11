20fec1818e0ae9cf99dc07a86465d0ab34f86894
a.v.zalesov <a.v.zalesov@tmn3.etagi.com>
2021-11-11 16:51:04 +0500

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const {MongoClient} = require('mongodb')
+const client = new MongoClient('mongodb+srv://Suplex:asdf321@cluster0.ugstu.mongodb.net/Heroku_testApp1?retryWrites=true&w=majority')
+
+const start = async () => {
+  try {
+    await client.connect()
+    console.log('MongoDB')
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+app.get('/', async(req, res) => {
+  await res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/items', async(req, res) => {
+  const items = await client.db('test').collection('items').find().toArray()
+  res.json(items)
+})
+
+app.get('/users', async(req, res) => {
+  const users = await client.db('test').collection('users').find().toArray()
+
+  res.json(users)
+})
+
+app.listen(3030, () => start())
